6d8cc3688173ffe4b73aec99b9e787d8eeb5f10a
정한비 <hanbijeong2@gmail.com>
2026-05-12 21:36:48 +0900

    Grade 클래스 수정(띄어쓰기, 엔터, 프로그램 정보 등)

--- Grade.java
+++ Grade.java
@@ -1,9 +1,9 @@
 
 /**
- * Grade 클래스의 설명을 작성하세요.
+ * 교수가 학생의 성적 정보를 직접 입력하고 관리할 수 있는 성적 처리 프로그램.
  *
- * @author (작성자 이름)
- * @version (버전 번호 또는 작성한 날짜)
+ * @author (2025320031 김단이, 2025320016 정한비, 2025320020 송영재)
+ * @version (2026.05.12)
  */
 public class Grade{
     private final String strGPA;
@@ -13,22 +13,23 @@ public class Grade{
         this.strGPA = strGPA;
         this.numGPA = numGPA;
     }
+    
     public String getStrGPA(){
         return strGPA;
     }
+    
     public double getNumGPA(){
         return numGPA;
     }
+    
     public static Grade scoreToGPA(int score){
         switch (score / 10){
             case 10:
-            case 9:return (score >= 95)? new Grade("A+",4.5) : new Grade("A",4.0);
-            case 8:return (score >= 85)? new Grade("B+",3.5) : new Grade("A",3.0);
-            case 7:return (score >= 75)? new Grade("C+",2.5) : new Grade("A",2.0);
-            case 6:return (score >= 65)? new Grade("D+",1.5) : new Grade("A",1.0);
-
-            default:
-                return new Grade("F",0.0);
+            case 9: return (score >= 95) ? new Grade("A+",4.5) : new Grade("A",4.0);
+            case 8: return (score >= 85) ? new Grade("B+",3.5) : new Grade("A",3.0);
+            case 7: return (score >= 75) ? new Grade("C+",2.5) : new Grade("A",2.0);
+            case 6: return (score >= 65) ? new Grade("D+",1.5) : new Grade("A",1.0);
+            default: return new Grade("F", 0.0);
         }
     }
 }

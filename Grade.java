12b51ca0d88ddbabb2787b606e480ac28950b265
정한비 <hanbijeong2@gmail.com>
2026-05-14 15:25:14 +0900

    Grade 오류 제거

--- Grade.java
+++ Grade.java
@@ -26,9 +26,9 @@ public class Grade{
         switch (score / 10){
             case 10:
             case 9: return (score >= 95) ? new Grade("A+",4.5) : new Grade("A",4.0);
-            case 8: return (score >= 85) ? new Grade("B+",3.5) : new Grade("A",3.0);
-            case 7: return (score >= 75) ? new Grade("C+",2.5) : new Grade("A",2.0);
-            case 6: return (score >= 65) ? new Grade("D+",1.5) : new Grade("A",1.0);
+            case 8: return (score >= 85) ? new Grade("B+",3.5) : new Grade("B",3.0);
+            case 7: return (score >= 75) ? new Grade("C+",2.5) : new Grade("C",2.0);
+            case 6: return (score >= 65) ? new Grade("D+",1.5) : new Grade("D",1.0);
             default: return new Grade("F", 0.0);
         }
     }

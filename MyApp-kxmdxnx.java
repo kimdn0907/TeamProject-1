23a4482c7452a3d71ff30a23ebe6d69330243213
김단이 <kimdn0907@gamil.com>
2026-05-15 12:37:28 +0900

    MyApp 코드 가독성 좋게 printAll과 main사이 엔터처리

--- MyApp-kxmdxnx.java
+++ MyApp-kxmdxnx.java
@@ -13,7 +13,6 @@ public class MyApp
             studentList[i].displayResult();
         }
     }
-    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
@@ -32,13 +31,13 @@ public class MyApp
                 sc.nextLine();
                 
                 //학생 이름 입력받기
-                System.out.print("학생 이름을 입력해주세요 : ");
+                System.out.print("학생 이름을 입력해주세요. : ");
                 String studentName = sc.nextLine();
                 
                 //학번 입력받기 (문자 입력 시 오류메세지 출력하고 다시 입력하게 하기)
                 int studentId;
                 while (true){
-                    System.out.print("학번을 입력해주세요 : ");
+                    System.out.print("학번을 입력해주세요. : ");
                     studentId = 0;
                     try {
                         studentId = sc.nextInt();
@@ -57,7 +56,7 @@ public class MyApp
                 
                 //과목 입력 받기 -> 0입력 시 과목 입력 받는 것을 종료하고 학생 정보 입력으로 돌아감
                 while (true){
-                    System.out.print("과목을 입력하시겠습니까? (1: 예 / 0: 종료) :");
+                    System.out.print("과목을 입력하시겠습니까? (1: 예 / 0: 종료)");
                     int addSubject = sc.nextInt();
                     if (addSubject == 0)
                         break;
@@ -65,13 +64,13 @@ public class MyApp
                         sc.nextLine();
                         
                         //과목 이름 입력받기
-                        System.out.print("과목 이름을 입력해주세요 : ");
+                        System.out.print("과목 이름을 입력해주세요. : ");
                         String subjectName = sc.nextLine();
                         
                         //과목에 대한 점수 입력받기 (0~100 사이의 입력을 벗어나면 오류메세지 출력 / 숫자가 아닌 입력을 받으면 오류메세지 출력)
                         while (true){
                             try {
-                                System.out.print("점수를 입력해 주세요 : ");
+                                System.out.print("점수를 입력해 주세요. : ");
                                 int score = sc.nextInt();
                                 sc.nextLine();
                                 if (score >= 0 && score <= 100){
@@ -96,7 +95,7 @@ public class MyApp
         }
         //전체 학생 성적 출력 후 전체 학생 수 출력 -> 전체 석차, 최고/최저, 평균점수 출력에 사용
         printAll(studentList, studentCount);
-        System.out.println("전체 학생 수는 " + Student.totalStudents + "명 입니다.");
+        System.out.println("전체 학생 수는" + Student.totalStudents + "명 입니다.");
         
         //더 이상 입력받을 것이 없으면 Scanner 종료하기
         sc.close();
